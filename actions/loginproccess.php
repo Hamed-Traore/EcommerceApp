2294ce5c2476a8274bdbd7bd3d650500e725255d
Hamed-Traore <91626326+Hamed-Traore@users.noreply.github.com>
2022-11-06 23:30:03 +0000

    tryout 3

--- actions/loginproccess.php
+++ actions/loginproccess.php
@@ -6,22 +6,25 @@ include ('../controllers/customer_controller.php');
 
 //2- Collect the data $GET and $POST
 if (isset($_POST['submit'])) {
+    echo "here";
     $customer_email= $_POST['customer_email'];
     $customer_pass= $_POST['customer_pass'];
-    // echo $customer_email . " " . $customer_pass;
-    $result = loginUser_controller($customer_email,$customer_pass);
-    if ($result===false) {
-        header( "Location: ../view/login.php?error=Please you typed a wrong Email or password");
-    }
-    else {
-        if ($_SESSION['user_role']==1) {
-            header( "Location: ../admin/dashboard.php");
-        }
-        else {
-            header( "Location: ../view/customer_dashboard.php");
-        }
+    
+    echo $customer_email . " " . $customer_pass;
+//     $result = loginUser_controller($customer_email,$customer_pass);
+//     var_dump($result);
+//     if ($result===false) {
+//         header( "Location: ../view/login.php?error=Please you typed a wrong Email or password");
+//     }
+//     else {
+//         if ($_SESSION['user_role']==1) {
+//             header( "Location: ../admin/dashboard.php");
+//         }
+//         else {
+//             header( "Location: ../view/customer_dashboard.php");
+//         }
         
-    }
+//     }
    
 }
 
